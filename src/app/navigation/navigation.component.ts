7e34a3d9ed1f2b14b20a4d73110b6fbb5db9bd73
hamoudih1 <hhamoudih1@gmail.com>
2019-08-24 10:04:18 -0400

    Nav bold depending on component

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  currentURL : string;
+
+  currentActive: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkPath();
   }
 
+  checkPath(){
+    this.currentURL = window.location.pathname;
+
+    if(this.currentURL.includes("location")) {
+      this.currentActive = 1;
+    }
+    else if(this.currentURL.includes("services")) {
+      this.currentActive = 2;
+    }
+    else if(this.currentURL.includes("staff")) {
+      this.currentActive = 3;
+    }
+    else if(this.currentURL.includes("date")) {
+      this.currentActive = 4;
+    }
+    else if(this.currentURL.includes("time")) {
+      this.currentActive = 5;
+    }
+    else if(this.currentURL.includes("review")) {
+      this.currentActive = 6;
+    }
+    else if(this.currentURL.includes("payment")) {
+      this.currentActive = 7;
+    }
+    else {
+      this.currentActive = 0;
+      console.log(this.currentURL);
+      
+    }
+  }
 }
